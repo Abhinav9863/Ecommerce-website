20c30d05f9b4954a91b703013b1149b6a87f9e77
abhinav9863 <abhinavvishnumohan21@gmail.com>
2025-07-06 22:17:44 +0530

    radio selectors

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -32,7 +32,7 @@ cart.forEach((cartItem) =>{
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                ${matchingProduct.priceCents / 100}
+                $${(matchingProduct.priceCents / 100).toFixed}
             </div>
             <div class="product-quantity">
                 <span>
@@ -46,7 +46,7 @@ cart.forEach((cartItem) =>{
                 </span>
             </div>
             </div>
-
+ 
             <div class="delivery-options">
             <div class="delivery-options-title">
                 Choose a delivery option:
@@ -54,7 +54,7 @@ cart.forEach((cartItem) =>{
             <div class="delivery-option">
                 <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Tuesday, June 21
@@ -67,7 +67,7 @@ cart.forEach((cartItem) =>{
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Wednesday, June 15
@@ -80,7 +80,7 @@ cart.forEach((cartItem) =>{
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Monday, June 13
